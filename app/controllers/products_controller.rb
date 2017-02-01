6b08bc4f5bcc05c5dc9f62a51b5f72b9f57b98d2
Priyanka <priyanka3007@gmail.com>
2017-01-31 16:51:00 -0800

    Completed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,38 @@ class ProductsController < ApplicationController
  		@products = Product.all
  	end 
 
+ 	def new
+ 		
+ 	end
+
+ 	def create
+ 		Product.create(name: params[:name], description: params[:description], pricing: params[:pricing])
+       redirect_to '/products'
+    end
+
+    def show
+    	@products = Product.find(params[:id])
+    	
+    end
+
+    def edit
+    	@products = Product.find(params[:id])
+    end
+
+    def update
+    	product = Product.find(params[:id])
+        product.name = params[:name]
+        product.description = params[:description]
+        product.pricing = params[:pricing]
+
+        product.save
+        redirect_to '/products'
+    end
+
+    def destroy
+    	product = Product.find(params[:id])
+    	product.destroy
+    	redirect_to "/products"
+    end
+
 end
